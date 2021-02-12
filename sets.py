6450dd7910c93f4e7fc10e443a42ecd385229221
Shishiranjan Thakur <34187897+Shishiranjan@users.noreply.github.com>
2021-02-12 21:31:39 +0545

    Add files via upload
    Sets are used to store multiple items in a single variable. Set is one of 4 built-in data types in Python used to store collections of data, the other 3 are List, Tuple, and Dictionary, all with different qualities and usage. A set is a collection which is both unordered and unindexed.

---
+++ sets.py
@@ -0,0 +1,11 @@
+A = {14, 21, 19, 32, 8}; 
+B = {1, 7, 19, 14, 21}; 
+  
+# union 
+print("Union :", A | B)
+
+#intersection
+print("Intersection :", A & B)
+
+#complement
+print("Complements :", A - B) 
